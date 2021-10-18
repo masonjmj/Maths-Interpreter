ec69a53cc19b80dbe2757f93effabf970fd7ecb6
James Mason <jamesmjmason@icloud.com>
2021-10-18 14:43:35 +0100

    Adjusted tests

--- code/src/com/group8/Main.java
+++ code/src/com/group8/Main.java
@@ -27,24 +27,33 @@ public class Main {
 		// Replace with proper unit testing
 
 		// Basic operations
-		System.out.println("TEST: 5 + 5. Expected output: 10");
-		Lexer.analyse("5 + 5");
+		System.out.println("TEST: 5. Expected output: 10");
+		Lexer.analyse("5");
 
-		System.out.println("TEST: 5 - 5. Expected output: 0");
-		Lexer.analyse("5 - 5");
+		System.out.println("TEST: 5+5. Expected output: 10");
+		Lexer.analyse("5+5");
 
-		System.out.println("TEST: 5 * 5. Expected output: 25");
-		Lexer.analyse("5 * 5");
+		System.out.println("TEST: 5-5. Expected output: 0");
+		Lexer.analyse("5-5");
 
-		System.out.println("TEST: 5 / 5. Expected output: 1");
-		Lexer.analyse("5 / 5");
+		System.out.println("TEST: 5*5. Expected output: 25");
+		Lexer.analyse("5*5");
 
-		System.out.println("TEST: 5 + (4 - 2 + 3). Expected output: 10");
-		Lexer.analyse("5 + (4 - 2 + 3)");
+		System.out.println("TEST: 5/5. Expected output: 1");
+		Lexer.analyse("5/5");
+
+		System.out.println("TEST: 5+(4-2+3). Expected output: 10");
+		Lexer.analyse("5+(4-2+3)");
+
+		System.out.println("TEST: 5   +   5. Expected output: 10");
+		Lexer.analyse("5   +   5"); // Whitespace ignored
 
 		// Errors
 		System.out.println("TEST: 5 + . Expected output: Syntax error");
 		Lexer.analyse("5 + ");
+
+		System.out.println("TEST: 5 + ( 5. Expected output: Syntax error");
+		Lexer.analyse("5 + ( 5");
 
 		System.out.println("TEST: 2147483648 + 1. Expected output: Runtime Exception");
 		Lexer.analyse("2147483648 + 1");
