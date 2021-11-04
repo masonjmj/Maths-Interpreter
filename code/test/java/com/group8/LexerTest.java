09d3480035cb62281f5307b84314aba4802b15c9
James Mason <jamesmjmason@icloud.com>
2021-11-04 15:05:06 +0000

    Set up testing framework.

---
+++ code/test/java/com/group8/LexerTest.java
@@ -0,0 +1,15 @@
+package com.group8;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LexerTest {
+
+	@Test
+	@DisplayName("Lexical Analysis")
+	void analyse() {
+
+	}
+}
